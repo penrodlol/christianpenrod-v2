f8efa606c6b571e10107e2b6a98089f32a9d1710
penrodlol <christian@penrod.contact>
2022-05-28 21:53:10 -0400

    🚧 Begining the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
+import { Box } from '@components/Box';
+import { Header } from '@components/Header';
 import { Hero } from '@components/Hero';
 import { Layout } from '@components/Layout';
+import { SocialLinks } from '@components/SocialLinks';
 
 const Home = () => {
   return (
     <Layout>
-      <Hero />
+      <Box flexContainer position="absolute" inset="0px" gap="4">
+        <Box
+          flex="60%"
+          flexContainer
+          flexDirection="column"
+          margin="4"
+          paddingLeft="4"
+          paddingBottom="8"
+        >
+          <Header />
+          <Box
+            flex="auto"
+            flexContainer
+            flexDirection="column"
+            justifyContent="space-between"
+            margin="0 auto"
+            marginTop="11"
+          >
+            <Hero />
+            <SocialLinks />
+          </Box>
+        </Box>
+        <Box flex="40%" height="100%" padding="4">
+          <Box bg="surface.1" size="inherit" borderRadius="3"></Box>
+        </Box>
+      </Box>
     </Layout>
   );
 };
