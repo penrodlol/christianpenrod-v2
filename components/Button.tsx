eae975f9d83bb16b7d220f6ce5b1847cfc117321
penrodlol <christian@penrod.contact>
2022-05-28 21:49:06 -0400

    💄 Revert some button size increases

--- components/Button.tsx
+++ components/Button.tsx
@@ -27,7 +27,7 @@ export const Button = styled.button<ButtonProps>`
       primary: {
         bg: 'brand.2',
         boxShadow: '3',
-        padding: 'var(--size-3) var(--size-8)',
+        padding: '0.8rem var(--size-7)',
         '&:hover:enabled': {
           bg: 'brand.1',
           boxShadow: '4',
@@ -37,7 +37,7 @@ export const Button = styled.button<ButtonProps>`
         bg: 'accent.2',
         color: 'surface.1',
         boxShadow: '3',
-        padding: 'var(--size-3) var(--size-8)',
+        padding: '0.8rem var(--size-7)',
         '&:hover:enabled': {
           bg: 'accent.1',
           boxShadow: '4',
