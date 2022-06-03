61723719a9b6e2f00b4e254a482f4d71eb935bb9
penrodlol <christian@penrod.contact>
2022-06-02 21:44:01 -0400

    🚧 Cards in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { Box } from '@components/Box';
+import { Card } from '@components/Card';
 import { Header } from '@components/Header';
 import { Hero } from '@components/Hero';
 import { Layout } from '@components/Layout';
 import { SocialLinks } from '@components/SocialLinks';
+import { Text } from '@components/Text';
+import ArrowRightIcon from '@svg/arrow-right.svg';
 
 const Home = () => {
   return (
@@ -32,7 +35,44 @@ const Home = () => {
           </Box>
         </Box>
         <Box as="section" flex="35%" flexContainer padding="4">
-          <Box bg="surface.1" size="100%" borderRadius="3"></Box>
+          <Box
+            bg="surface.1"
+            size="100%"
+            borderRadius="3"
+            flexContainer
+            flexDirection="column"
+            gap="8"
+            paddingX="8"
+            borderY="solid var(--size-4) var(--surface-1)"
+            boxShadow="4"
+            overflowY="auto"
+          >
+            {[
+              'Developing Browser Extensions with Next.js',
+              'Hyper Terminal and its arsenal of plugins',
+              'Local State with NgRx & Apollo Angular',
+            ].map((title) => (
+              <Card
+                key={title}
+                title={title}
+                subTitle="April 19th, 2022"
+                tags={['Angular', 'Next.js', 'Supabase']}
+                content="Browser extensions are fantastic tools for enhancing web development and general
+                productivity. Lets find out how to create one while leveraging the power of
+                Next.js!"
+                actions={[
+                  <Box key={title + 'read more'} flexContainer gap="2">
+                    <Text fontSize="2">Read More</Text>
+                    <ArrowRightIcon
+                      width={25}
+                      height={25}
+                      fill="var(--brand-1)"
+                    />
+                  </Box>,
+                ]}
+              />
+            ))}
+          </Box>
         </Box>
       </Box>
     </Layout>
