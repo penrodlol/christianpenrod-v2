e5f2730b6e2ef153e96dd0d922103d4cfcaf5847
penrodlol <christian@penrod.contact>
2022-06-02 21:41:25 -0400

    🏗️ Line "height" prop

--- components/Line.tsx
+++ components/Line.tsx
@@ -3,13 +3,14 @@ import { Box } from './Box';
 
 export interface LineProps {
   size?: string;
+  height?: string;
 }
 
-export const Line: FC<LineProps> = ({ size }) => (
+export const Line: FC<LineProps> = ({ size, height }) => (
   <Box
     backgroundColor="brand.1"
     borderRadius="2"
-    height="2"
+    height={height || '2'}
     marginY={size || '3'}
   />
 );
