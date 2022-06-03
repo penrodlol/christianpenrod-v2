e499eea2b3b4c099d722b663e1e0e6ca173ad028
penrodlol <christian@penrod.contact>
2022-06-02 21:40:58 -0400

    ✨ Chip

---
+++ components/Chip.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from 'react';
+import { Box } from './Box';
+import { Text } from './Text';
+
+export const Chip = ({ children }: PropsWithChildren<{}>) => {
+  return (
+    <Box bg="accent.2" color="surface.1" borderRadius="round" paddingX="2">
+      <Text letterSpacing="2" lineHeight="4" fontSize="0.9rem">
+        {children}
+      </Text>
+    </Box>
+  );
+};
