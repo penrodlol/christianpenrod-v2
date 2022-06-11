4406cf3bf2baee07c03aa3cdb5a3e3aa052f7dea
penrodlol <christian@penrod.contact>
2022-06-11 00:05:10 -0400

    🏗️ dayjs plugin config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import dayjs from 'dayjs';
+import advancedFormatPlugin from 'dayjs/plugin/advancedFormat';
+import utcPlugin from 'dayjs/plugin/utc';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { THEME } from '../const/theme';
 import '../styles/globals.scss';
+
+dayjs.extend(advancedFormatPlugin);
+dayjs.extend(utcPlugin);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
