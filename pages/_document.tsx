251a81d63771a754ca19d6099da99484c527a9ca
penrodlol <christian@penrod.contact>
2022-06-12 14:24:29 -0400

    🏗️ Pre-render styled-component classes

---
+++ pages/_document.tsx
@@ -0,0 +1,50 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { Fragment } from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: [
+          <Fragment key="38d9b3f0-6985-48b2-86b9-96fc49035b1b">
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
