92fb80c39decc898356882c80be2e75f8cb41652
penrodlol <christian@penrod.contact>
2022-06-12 21:35:34 -0400

    🏗️ Should be mdx

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -6,13 +6,17 @@ import {
   InferGetStaticPropsType,
   NextPage,
 } from 'next';
+import { useMDXComponent } from 'next-contentlayer/hooks';
 
 const Blog: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   post,
 }) => {
+  const MDXContent = useMDXComponent(post.body.code);
+
   return (
     <Layout>
       <h1>{post.title}</h1>
+      <MDXContent />
     </Layout>
   );
 };
