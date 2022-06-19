f437a87a36e926ea5b5560f678dd1365bab63dee
penrodlol <christian@penrod.contact>
2022-06-18 20:33:36 -0400

    ♻️ Anchor

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -1,21 +1,14 @@
 import styled from 'styled-components';
 import { Text } from './Text';
 
-export interface AnchorProps {
-  external?: boolean;
-}
-
-export const Anchor = styled(Text.withComponent('a')).attrs<AnchorProps>(
-  ({ external, ...props }) => ({
-    ...props,
-    target: external ? '_blank' : undefined,
-    rel: external ? 'noopener noreferrer' : undefined,
-  }),
-)`
-  color: var(--text-1);
+export const Anchor = styled(Text.withComponent('a'))`
   position: relative;
   text-decoration: none;
   border-radius: var(--radius-2);
+
+  &[target='_blank'] {
+    color: var(--brand-1);
+  }
 
   &:hover {
     color: var(--brand-1);
