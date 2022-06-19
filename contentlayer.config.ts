d5e0819c785419dbd11a55c7b895ab3f7c790186
penrodlol <christian@penrod.contact>
2022-06-18 20:33:23 -0400

    ➕ A few rehype/remark plugins and headings prop

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,9 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 import readingTime from 'reading-time';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import remarkPrism from 'remark-prism';
+import slugify from 'slugify';
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -23,11 +26,23 @@ export const Post = defineDocumentType(() => ({
       type: 'number',
       resolve: (post) => Math.round(readingTime(post.body.code).minutes),
     },
+    headings: {
+      type: 'list',
+      of: { type: 'json' },
+      resolve: (post) =>
+        post.body.raw.match(/##.+/g)?.map((h2) => {
+          h2 = h2.replace(/##/, '').trim();
+          return { text: h2, hash: slugify(h2, { lower: true }) };
+        }),
+    },
   },
 }));
 
 export default makeSource({
   contentDirPath: 'posts',
   documentTypes: [Post],
-  mdx: { rehypePlugins: [rehypeExternalLinks] },
+  mdx: {
+    rehypePlugins: [rehypeExternalLinks, rehypeSlug],
+    remarkPlugins: [remarkPrism],
+  },
 });
