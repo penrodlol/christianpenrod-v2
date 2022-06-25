a204967f21940c7a7eaa650e7353abf81ccd15e5
penrodlol <christian@penrod.contact>
2022-06-25 14:02:41 -0400

    💄 External anchor underline on hover

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -6,11 +6,21 @@ export const Anchor = styled(Text.withComponent('a'))`
   text-decoration: none;
   border-radius: var(--radius-2);
 
-  &[target='_blank'] {
+  &:hover {
     color: var(--brand-1);
   }
 
-  &:hover {
+  &[target='_blank'] {
     color: var(--brand-1);
+
+    &:hover:after {
+      content: '';
+      position: absolute;
+      right: 0;
+      left: 0;
+      bottom: 2px;
+      border-top: 0.2rem solid var(--brand-1);
+      border-radius: var(--radius-2);
+    }
   }
 `;
