30edf9cfefc325f15e349222c20d67834673567c
penrodlol <christian@penrod.contact>
2022-07-17 20:41:48 -0400

    🐛 Wrong env var

--- pages/api/repo-meta/[repo].ts
+++ pages/api/repo-meta/[repo].ts
@@ -3,7 +3,7 @@ import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
 
 export default async function handler(req: Req, res: Res) {
   const repo = String(req.query.repo);
-  const owner = String(process.env.GITHUB_USER_NAME);
+  const owner = String(process.env.GITHUB_USERNAME);
   const payload = await repos.get({ repo, owner });
 
   res.status(200).json({ ...payload.data });
