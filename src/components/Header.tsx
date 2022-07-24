c94b7ec00d5c970b6cc3540f665d2f39421f6fb5
penrodlol <christian@penrod.contact>
2022-07-24 13:54:34 -0400

    🏗️ Move "Hamburger" into Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import LogoIcon from '@svg/logo.svg';
 import Route from 'next/link';
+import { useCallback, useRef } from 'react';
 import { Anchor } from './Anchor';
-import { Hamburger } from './Hamburger';
 
 export const Header = () => {
   return (
@@ -16,9 +16,7 @@ export const Header = () => {
           <Routes />
         </div>
         <div className="block sm:hidden">
-          <Hamburger>
-            <Routes />
-          </Hamburger>
+          <RoutesMenu />
         </div>
       </nav>
     </header>
@@ -36,3 +34,41 @@ const Routes = () => (
     ))}
   </>
 );
+
+const RoutesMenu = () => {
+  const hamburger = useRef<HTMLInputElement>(null);
+  const toggleOverflow = useCallback(
+    () => document.body.classList.toggle('overflow-hidden'),
+    [],
+  );
+
+  return (
+    <div className="relative w-10 h-6">
+      <input
+        ref={hamburger}
+        className="peer absolute z-20 inset-0 w-full h-full cursor-pointer opacity-0"
+        type="checkbox"
+        aria-label="Toggle menu to see routes"
+        onChange={() => toggleOverflow()}
+      />
+      <span
+        className="absolute z-10 top-0 w-full h-[0.45rem] rounded-md bg-brand-1
+                   peer-checked:top-1/2 peer-checked:-translate-y-1/2 peer-checked:rotate-45"
+      />
+      <span
+        className="absolute z-10 bottom-0 w-full h-[0.45rem] rounded-md bg-brand-2
+                   peer-checked:top-1/2 peer-checked:-translate-y-1/2 peer-checked:-rotate-45"
+      />
+      <menu
+        className="hidden fixed inset-0 backdrop-blur-md peer-checked:flex flex-col
+                   gap-8 px-8 pt-16 text-4xl"
+        onClick={() => {
+          hamburger.current!.checked = false;
+          toggleOverflow();
+        }}
+      >
+        <Routes />
+      </menu>
+    </div>
+  );
+};
