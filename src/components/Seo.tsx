419602918fa74f474229100cc55a399ffd281c69
penrodlol <christian@penrod.contact>
2022-07-31 21:36:10 -0400

    🚧 Continue w/ SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 const NAME = 'Christian Penrod';
 const TITLE = `${NAME} | Front-End Web Developer`;
 const DESC = 'Developing for the web since 2015, based in Pittsburgh PA.';
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+const TWITTER = process.env.NEXT_PUBLIC_TWITTER;
 
 export interface SeoProps {
   title?: string;
@@ -11,12 +14,32 @@ export interface SeoProps {
 }
 
 export const SEO: FC<SeoProps> = ({ title, description }) => {
+  const { asPath } = useRouter();
+
   return (
     <Head>
       <title>{title ? `${title} | ${NAME}` : TITLE}</title>
 
       <meta name="title" content={title ?? TITLE} />
       <meta name="description" content={description ?? DESC} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={`@${TWITTER}`} />
+      <meta name="twitter:title" content={title || TITLE} />
+      <meta name="twitter:description" content={description || DESC} />
+      {/* <meta name="twitter:image" content={``} /> */}
+
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content={title || TITLE} />
+      <meta name="og:description" content={description || DESC} />
+      {/* <meta name="og:image" content={``} /> */}
+      <meta name="og:image:alt" content={`Banner for ${DOMAIN}`} />
+      <meta name="og:image:width" content="1280" />
+      <meta name="og:image:height" content="675" />
+
+      {/* <link href="" rel="icon" type="image/x-icon" /> */}
+
+      <link href={`${DOMAIN}${asPath}`} rel="canonical" />
     </Head>
   );
 };
