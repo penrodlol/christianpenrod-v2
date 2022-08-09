474c1eb7bfddc198e01f49acd540e06514296742
penrodlol <christian@penrod.contact>
2022-08-08 22:46:42 -0400

    ♿️ Make external links apparent

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,9 +1,10 @@
+import ExternalIcon from '@svg/external.svg';
 import { AnchorHTMLAttributes, ForwardedRef, forwardRef } from 'react';
 
 export type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
 
 const external =
-  'hover:after:content-blank hover:after:absolute hover:after:right-0 ' +
+  'text-brand-1 hover:after:content-blank hover:after:absolute hover:after:right-0 ' +
   'hover:after:left-0 hover:after:bottom-[2px] hover:after:border-t-[0.2rem] ' +
   'hover:after:border-solid hover:after:border-brand-1 hover:after:rounded-md';
 
@@ -15,10 +16,11 @@ const AnchorComponent = (
     {...props}
     ref={ref}
     target={target || '_self'}
-    className={`relative no-underline rounded-md hover:text-brand-1
+    className={`inline-flex gap-1 items-center relative no-underline rounded-md hover:text-brand-1
                 ${className} ${target === '_blank' && external}`}
   >
-    {children}
+    <span>{children}</span>
+    {target === '_blank' && <ExternalIcon className="h-5 w-5" />}
   </a>
 );
 
