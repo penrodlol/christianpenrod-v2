84eb037686d293f92c096c49455efd02641a5605
penrodlol <christian@penrod.contact>
2022-08-13 20:19:37 -0400

    ✨ post views upsert

--- src/pages/api/post-views/[slug].ts
+++ src/pages/api/post-views/[slug].ts
@@ -2,9 +2,12 @@ import { prisma } from '@utils/prisma';
 import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
 
 export default async function handler(req: Req, res: Res<number>) {
-  const { views } = await prisma.postView.update({
-    data: { views: { increment: 1 } },
-    where: { post: String(req.query.slug) },
+  const post = String(req.query.slug);
+
+  const { views } = await prisma.postView.upsert({
+    update: { views: { increment: 1 } },
+    create: { post },
+    where: { post },
   });
 
   res.status(200).json(views);
