3e9cec769675a435a75f4d5cdfe07bace1737cdd
penrodlol <christian@penrod.contact>
2022-08-13 20:24:39 -0400

    ✨ Button forwardRef

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,19 @@
-import { ButtonHTMLAttributes, FC } from 'react';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: FC<ButtonProps> = ({ children, className, ...props }) => (
-  <button
-    {...props}
-    className={`
-      bg-accent-2 text-[black] shadow-2 hover:bg-accent-1 text-xl font-extrabold
-      rounded-md px-8 py-[0.8rem]  ${className}`}
-  >
-    {children}
-  </button>
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, ...props }, ref) => (
+    <button
+      {...props}
+      ref={ref}
+      className={`
+        bg-accent-2 text-[black] shadow-2 hover:bg-accent-1 text-xl font-extrabold
+        rounded-md px-8 py-[0.8rem]  ${className}`}
+    >
+      {children}
+    </button>
+  ),
 );
+
+Button.displayName = 'Button';
