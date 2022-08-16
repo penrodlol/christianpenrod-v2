760ab6eca9755d6ce89005638cf2849042b7bd45
penrodlol <christian@penrod.contact>
2022-08-15 21:09:27 -0400

    ⏪ Revent PostGithub hover card

--- src/components/PostGithub.tsx
+++ src/components/PostGithub.tsx
@@ -5,6 +5,7 @@ import { ReposGet } from '@utils/octokit';
 import Link from 'next/link';
 import { FC } from 'react';
 import useSWR from 'swr';
+import { Anchor } from './Anchor';
 
 export interface PostGithubProps {
   name: string;
@@ -18,43 +19,43 @@ export const PostGithub: FC<PostGithubProps> = ({ name }) => {
   );
 
   return (
-    <Link href={repo?.html_url || ''} passHref>
-      <a
-        target="_blank"
-        rel="nofollow noopener noreferrer"
-        className="group bg-surface-2 shadow-2 rounded-md p-5
-                 hover:outline hover:outline-2 hover:outline-surface-1 hover:outline-offset-4"
-      >
-        <div className="flex gap-4 items-center text-brand-1">
-          <GithubBookmarkIcon width={25} height={25} />
-          <span className="text-xl text-base-1 group-hover:text-brand-1">
-            {repo?.name || '--'}
+    <div className="bg-surface-2 shadow-2 rounded-md p-5">
+      <div className="flex gap-4 items-center text-brand-1">
+        <GithubBookmarkIcon width={25} height={25} />
+        <Link href={repo?.html_url || ''} passHref>
+          <Anchor
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            className="text-xl text-base-1"
+            aria-label={`Go to ${repo?.name}`}
+          >
+            {repo?.name}
+          </Anchor>
+        </Link>
+      </div>
+      <p className="text-base-2 font-semibold mt-4 mb-5">
+        {repo?.description || '--'}
+      </p>
+      <div className="flex gap-4 items-center">
+        <div className="flex gap-2 items-center mr-4">
+          <div className="bg-accent-2 w-4 h-4 rounded-full" />
+          <span className="text-base-2 font-semibold">
+            {repo?.language || '--'}
           </span>
         </div>
-        <p className="text-base-2 font-semibold mt-4 mb-5">
-          {repo?.description || '--'}
-        </p>
-        <div className="flex gap-4 items-center">
-          <div className="flex gap-2 items-center mr-4">
-            <div className="bg-accent-2 w-4 h-4 rounded-full" />
-            <span className="text-base-2 font-semibold">
-              {repo?.language || '--'}
-            </span>
-          </div>
-          <div className="flex gap-2 items-center text-brand-1">
-            <GithubStarIcon width={20} height={20} />
-            <span className="text-base-2 font-semibold">
-              {repo?.stargazers_count || 0}
-            </span>
-          </div>
-          <div className="flex gap-2 items-center text-brand-1">
-            <GithubForkIcon width={20} height={20} />
-            <span className="text-base-2 font-semibold">
-              {repo?.forks_count || 0}
-            </span>
-          </div>
+        <div className="flex gap-2 items-center text-brand-1">
+          <GithubStarIcon width={20} height={20} />
+          <span className="text-base-2 font-semibold">
+            {repo?.stargazers_count || 0}
+          </span>
         </div>
-      </a>
-    </Link>
+        <div className="flex gap-2 items-center text-brand-1">
+          <GithubForkIcon width={20} height={20} />
+          <span className="text-base-2 font-semibold">
+            {repo?.forks_count || 0}
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
