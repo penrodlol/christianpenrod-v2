dcd18caa0712f4c47f17aa396ce074f7cd16be66
penrodlol <christian@penrod.contact>
2022-08-17 22:25:12 -0400

    🚧 Making a hover card (may revert this...)

--- src/components/ContributionCard.tsx
+++ src/components/ContributionCard.tsx
@@ -1,7 +1,7 @@
 import { Contribution } from '@prisma/client';
 import GithubBookmarkIcon from '@svg/github-bookmark.svg';
+import Link from 'next/link';
 import { FC } from 'react';
-import { Anchor } from './Anchor';
 
 export interface ContributionCardProps {
   contribution: Partial<Contribution>;
@@ -10,21 +10,21 @@ export interface ContributionCardProps {
 export const ContributionCard: FC<ContributionCardProps> = ({
   contribution,
 }) => (
-  <div className="flex flex-col gap-1 bg-surface-2 shadow-2 rounded-md py-3 px-5">
-    <div className="flex justify-between">
-      <div className="flex gap-2 items-center text-lg">
-        <GithubBookmarkIcon className="h-5 w-5 fill-brand-1" />
-        <Anchor
-          href={contribution.url}
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-          className="text-base-1"
-          aria-label={`Go to github repo for ${contribution.name}`}
-        >
-          {contribution.name}
-        </Anchor>
+  <Link href={contribution.url!} passHref>
+    <a
+      className="group flex flex-col gap-1 bg-surface-2 shadow-2 rounded-md py-3 px-5
+                 hover:outline hover:outline-2 hover:outline-surface-1 hover:outline-offset-4"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+      aria-label={`Go to github repo for ${contribution.name}`}
+    >
+      <div className="flex justify-between">
+        <div className="flex gap-2 items-center text-lg">
+          <GithubBookmarkIcon className="h-5 w-5 fill-brand-1" />
+          <span className="group-hover:text-brand-1">{contribution.name}</span>
+        </div>
       </div>
-    </div>
-    <p className="text-base-2">{contribution.description}</p>
-  </div>
+      <p className="text-base-2">{contribution.description}</p>
+    </a>
+  </Link>
 );
