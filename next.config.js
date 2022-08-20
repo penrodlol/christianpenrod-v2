e00b13489f1811edb13272925657628c5ab9bd97
penrodlol <christian@penrod.contact>
2022-08-20 19:54:57 -0400

    ✨ Footer stats

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+const { withContentlayer } = require('next-contentlayer');
+const dayjs = require('dayjs');
+
+dayjs.extend(require('dayjs/plugin/advancedFormat'));
+dayjs.extend(require('dayjs/plugin/utc'));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = require('next-contentlayer').withContentlayer({
+const nextConfig = withContentlayer({
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
+  env: {
+    DOMAIN: 'https://christianpenrod.com',
+    TWITTER: 'https://twitter.com/penrodlol',
+    GITHUB: 'https://github.com/penrodlol',
+    LINKEDIN: 'https://linkedin.com/in/christian-penrod-07618314b/',
+    COPYRIGHT_YEAR: String(dayjs().year()),
+    LAST_UPDATE: String(dayjs().format('MMM Do, YYYY h:mm A')),
   },
+  i18n: { locales: ['en'], defaultLocale: 'en' },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,74 @@
 import GithubIcon from '@svg/github.svg';
 import LinkedInIcon from '@svg/linkedin.svg';
 import TwitterIcon from '@svg/twitter.svg';
-import dayjs from 'dayjs';
+import Link from 'next/link';
 import { ReactElement } from 'react';
-
-const TWITTER = process.env.NEXT_PUBLIC_TWITTER!;
-const GITHUB = process.env.NEXT_PUBLIC_GITHUB!;
-const LINKEDIN = process.env.NEXT_PUBLIC_LINKEDIN!;
+import { Anchor } from './Anchor';
 
 // prettier-ignore
 const SOCIALS: Array<{ name: string; url: string; icon: ReactElement }> = [
-  { name: 'Twitter', url: TWITTER, icon: <TwitterIcon aria-hidden focusable="false" /> },
-  { name: 'Github', url: GITHUB, icon: <GithubIcon aria-hidden focusable="false" /> },
-  { name: 'LinkedIn', url: LINKEDIN, icon: <LinkedInIcon aria-hidden focusable="false" /> },
+  { name: 'Twitter', url: process.env.TWITTER!, icon: <TwitterIcon aria-hidden focusable="false" /> },
+  { name: 'Github', url: process.env.GITHUB!, icon: <GithubIcon aria-hidden focusable="false" /> },
+  { name: 'LinkedIn', url: process.env.LINKEDIN!, icon: <LinkedInIcon aria-hidden focusable="false" /> },
 ];
 
 export const Footer = () => (
   <footer
-    className="relative bg-surface-3 py-2 before:content-blank before:absolute
+    className="relative bg-surface-3 pt-6 pb-4 px-4 before:content-blank before:absolute
                before:inset-0 before:shadow-1 before:rotate-180 before:z-10"
   >
-    <div className="relative flex flex-col gap-2 items-center z-20">
-      <div className="flex gap-16 py-2 max-w-max">
+    <div className="relative flex flex-col gap-6 font-semibold text-2 text-base-2 z-20">
+      <div className="flex gap-20 justify-center">
         {SOCIALS.map(({ name, url, icon }) => (
           <a
             key={name}
             href={url}
-            target="_blank"
-            rel="nofollow noopener noreferrer"
             title={name}
-            className="w-7 h-7 cursor-pointer rounded-sm
+            aria-label={name}
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+            className="text-base-1 w-7 h-7 cursor-pointer rounded-sm
                        hover:text-brand-1 focus-visible:text-brand-1"
           >
             {icon}
-            <span className="sr-only">{name}</span>
           </a>
         ))}
       </div>
-      <div className="flex gap-2 text-xl">
-        <span className="font-semibold">Christian Penrod</span>
-        <span className="text-accent-2 font-bold">&#169;{dayjs().year()}</span>
+      <div className="flex flex-col gap-2 items-center">
+        <div className="flex gap-2">
+          <span>Created by: Christian Penrod</span>
+          <strong className="text-base-1">
+            &#169;{process.env.COPYRIGHT_YEAR}
+          </strong>
+        </div>
+        <div>
+          <span className="mr-2">Built with:</span>
+          <Link href="https://nextjs.org" passHref>
+            <Anchor
+              className="text-base-1 font-bold"
+              rel="nofollow noopener noreferrer"
+              target="_blank"
+            >
+              Next.js
+            </Anchor>
+          </Link>
+          <span className="mx-2">on</span>
+          <Link href="https://www.netlify.com/" passHref>
+            <Anchor
+              className="text-base-1 font-bold"
+              rel="nofollow noopener noreferrer"
+              target="_blank"
+            >
+              Netlify
+            </Anchor>
+          </Link>
+        </div>
+        <div>
+          <span className="mr-2">Updated on:</span>
+          <strong className="text-base-1 font-bold">
+            {process.env.LAST_UPDATE}
+          </strong>
+        </div>
       </div>
     </div>
   </footer>
