747c7d6c6b3e2bf6b155697466e17441913a11c1
penrodlol <christian@penrod.contact>
2022-08-28 21:29:35 -0400

    🐛 Needed for netlify forms to find form in JSX

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,6 +29,7 @@ export const Contact = () => {
             </button>
           </div>
           <form ref={form} name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
             <div className="flex flex-col gap-5 px-3 text-lg">
               <div className="flex flex-col sm:flex-row gap-5">
                 <Input
