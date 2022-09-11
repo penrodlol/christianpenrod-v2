e6a119c234aae7c5d12449280eb892a647529fa6
penrodlol <christian@penrod.contact>
2022-09-10 20:14:30 -0400

    🐛 forgot to update this...

--- src/server/db/octokit.ts
+++ src/server/db/octokit.ts
@@ -1,10 +1,13 @@
-import { Octokit } from '@octokit/rest';
+import { graphql } from '@octokit/graphql';
 
 declare global {
-  var octokit: Octokit | undefined;
+  var octokit: typeof graphql | undefined;
 }
 
 export const octokit =
-  global.octokit || new Octokit({ auth: process.env.GITHUB_TOKEN });
+  global.octokit ||
+  graphql.defaults({
+    headers: { authorization: `token ${process.env.GITHUB_TOKEN}` },
+  });
 
 if (process.env.NODE_ENV !== 'production') global.octokit = octokit;
