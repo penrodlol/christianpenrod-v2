73ee53663bb9f70753d533e4540b09531c695664
penrodlol <christian@penrod.contact>
2022-09-22 19:38:48 -0400

    💄 selection colors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ export const Layout = (props: LayoutProps) => {
   return (
     <>
       <SEO title={props.title} description={props.description} />
-      <div className="relative z-20 flex flex-col min-h-screen">
+      <div className="relative z-20 flex flex-col min-h-screen selection:bg-1">
         <Header />
         <main className="flex-1 px-fluid-4 max-w-screen-2xl mx-auto my-[clamp(1.31rem,calc(-0.52rem+9.15vw),6.00rem)]">
           {!props.tabOnly && props.title && (
