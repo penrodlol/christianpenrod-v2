62305b2a2aa33a1c618612400041a2daee07994d
penrodlol <christian@penrod.contact>
2022-09-22 20:08:35 -0400

    ✨ Trying out plausible

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const { withContentlayer } = require('next-contentlayer');
+const { withPlausibleProxy } = require('next-plausible');
 
 /** @type {import('next').NextConfig} */
-module.exports = withContentlayer({
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: { locales: ['en'], defaultLocale: 'en' },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
+module.exports = withPlausibleProxy()(
+  withContentlayer({
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: { locales: ['en'], defaultLocale: 'en' },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
 
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import env from '@env/client';
 import { AppRouter } from '@server/routers/_app';
 import { withTRPC } from '@trpc/next';
+import PlausibleProvider from 'next-plausible';
 import type { AppType } from 'next/dist/shared/lib/utils';
 import '../styles/globals.scss';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain={env.URL as string}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 };
 
 export default withTRPC<AppRouter>({
