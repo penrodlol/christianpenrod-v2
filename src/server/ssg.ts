ef3d668ace6729bb4411a49e849598986767b0f8
penrodlol <christian@penrod.contact>
2022-09-26 21:33:41 -0400

    🔥 Not used

--- src/server/ssg.ts
+++
@@ -1,6 +0,0 @@
-import { router } from '@server/routers/_app';
-import { createSSGHelpers } from '@trpc/react/ssg';
-import { ctx } from './context';
-
-export const createSSG = async () =>
-  createSSGHelpers({ router, ctx: await ctx() });
