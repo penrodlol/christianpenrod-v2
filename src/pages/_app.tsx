8f101896a1ef093c6d72666ea8fc4b382eb585fb
penrodlol <christian@penrod.contact>
2022-10-21 22:01:48 -0400

    ♻️ Remove scroll.scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import '@fontsource/nunito';
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/sriracha';
 import { trpc } from '@utils/trpc';
 import { AppType } from 'next/app';
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
 
 const MyApp: AppType = ({ Component, pageProps }) => (
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-function fromVars(name, size, pre) {
-  return [...Array(size + 1)]
-    .map((_, i) => `var(--${name}-${i})`)
-    .reduce((a, c, i) => ({ ...a, [[pre ? `${pre}-${i}` : i]]: c }), {});
-}
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
@@ -20,7 +14,15 @@ module.exports = {
         brand: { 1: '#b4befe', 2: '#b7bdf8', 3: '#babbf1' },
         accent: { 1: '#e3d6f5', 2: '#ddc9f8' },
       },
-      spacing: fromVars('size-fluid', 7, 'fluid'),
+      spacing: {
+        'fluid-1': 'clamp(0.25rem, calc(-0.09rem + 1.71vw), 1.13rem)',
+        'fluid-2': 'clamp(0.5rem, calc(0.11rem + 1.95vw), 1.5rem)',
+        'fluid-3': 'clamp(0.75rem, calc(0.16rem + 2.93vw), 2.25rem)',
+        'fluid-4': 'clamp(1rem, calc(0.22rem + 3.9vw), 3rem)',
+        'fluid-5': 'clamp(1.5rem, calc(0.33rem + 5.85vw), 4.5rem)',
+        'fluid-6': 'clamp(2rem, calc(0.44rem + 7.8vw), 6rem)',
+        'fluid-7': 'clamp(3rem, calc(0.66rem + 11.71vw), 9rem)',
+      },
       outlineColor: ({ theme }) => ({ DEFAULT: theme('backgroundColor.1') }),
       backgroundImage: ({ theme }) => ({
         'hero-pattern': `
@@ -35,6 +37,18 @@ module.exports = {
     require('tailwindcss-elevation')(['responsive']),
     ({ addBase, theme }) =>
       addBase({
+        '*, *::before, *::after': {
+          scrollbarWidth: 'thin',
+          scrollbarColor: `${theme('textColor.3')} transparent`,
+          '&::-webkit-scrollbar': {
+            width: theme('width.1'),
+            height: theme('height.1'),
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: theme('textColor.3'),
+            borderRadius: theme('borderRadius.md'),
+          },
+        },
         body: {
           fontFamily: theme('fontFamily.serif'),
           fontWeight: theme('fontWeight.extrabold'),
