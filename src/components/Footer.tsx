e9ac8b5f3f5a0ee4f69fe8b291f8901390f9a1b0
penrodlol <christian@penrod.contact>
2022-10-23 15:26:26 -0400

    💄 small tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ export const Footer = () => (
     className="relative bg-3 pt-6 pb-4 px-4 before:content-[''] before:absolute
                before:inset-0 before:elevation-5 before:rotate-180 before:z-10"
   >
-    <div className="relative flex flex-col gap-6 font-semibold text-base text-2 z-20">
-      <div className="flex gap-24 justify-center">
+    <div className="relative flex flex-col gap-6 font-semibold text-sm text-2 z-20">
+      <div className="flex gap-20 justify-center">
         <SocialAnchor name="Twitter" url={String(env.TWITTER)}>
           <Twitter aria-hidden focusable="false" />
         </SocialAnchor>
@@ -67,6 +67,8 @@ const SocialAnchor: FC<{
     className="text-1 cursor-pointer rounded-sm
                hover:text-brand-2 focus-visible:text-brand-2"
   >
-    {cloneElement(children, { className: 'stroke-current h-6 w-6' })}
+    {cloneElement(children, {
+      className: 'stroke-current fill-current h-6 w-6',
+    })}
   </a>
 );
