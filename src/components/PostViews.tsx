8401dc25ec52678580a5295de4a4f01ac6de3bb8
penrodlol <christian@penrod.contact>
2022-10-24 19:44:51 -0400

    🚧 testing something...

--- src/components/PostViews.tsx
+++ src/components/PostViews.tsx
@@ -15,7 +15,10 @@ export const PostViews: FC<PostViewsProps> = ({ slug }) => {
   );
 
   return (
-    <div className="flex gap-2 items-center bg-2 rounded-md elevation-10 text-brand-2 py-3 px-5">
+    <div
+      className="flex gap-2 items-center bg-2 rounded-md elevation-10 text-brand-2 py-3 px-5"
+      onClick={async () => fetch(`/api/post/like/${slug}`, { method: 'POST' })}
+    >
       <Eye className="w-6 h-6 stroke-brand-2" strokeWidth="3" />
       <span className="text-1 text-base tracking-widest">
         {String(views ?? 0).padStart(6, '0')}
--- src/env/server.js
+++ src/env/server.js
@@ -3,6 +3,7 @@ const env = {
   DATABASE_URL: process.env.DATABASE_URL,
   GITHUB_USERNAME: process.env.GITHUB_USERNAME,
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  SHA_256: process.env.SHA_256,
 };
 
 export default env;
