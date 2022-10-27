1f6eeb970d2d1abf923ed7749cb38aab663fe14a
penrodlol <christian@penrod.contact>
2022-10-26 21:17:12 -0400

    ✨ Career Page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,9 +1,13 @@
+import { RolesTimeline } from '@ui/RoleMDX';
 import { Title } from '@ui/Title';
 
 const CareerPage = async () => {
   return (
     <div>
       <Title title="Career" subtitle="What has Christian done?" />
+      <section>
+        <RolesTimeline />
+      </section>
     </div>
   );
 };
