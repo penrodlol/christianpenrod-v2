631667e332e598151453ba454f4b3aa6422ae1ae
penrodlol <christian@penrod.contact>
2022-11-01 19:49:47 -0400

    ⏪ Revert last

--- src/pages/api/views/[slug].ts
+++ src/pages/api/views/[slug].ts
@@ -2,10 +2,7 @@ import { prisma } from '@utils/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 
-export const config = { runtime: 'experimental-edge' };
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const payload = z.string().safeParse(req.query.slug);
 
   if (!payload.success) return res.status(400).json({ error: 'Invalid post' });
@@ -20,3 +17,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   res.status(200).json(views);
 };
+
+export default handler;
