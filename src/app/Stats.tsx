a0a01b3a82f84a044609325af4d1191f084cf9a0
penrodlol <christian@penrod.contact>
2022-12-01 21:06:52 -0500

    ♻️ Remove by accident

--- src/app/Stats.tsx
+++ src/app/Stats.tsx
@@ -23,13 +23,13 @@ export const Stats = async () => {
       <div className="flex flex-col gap-1">
         <Stat label="Total PRs:" data={user.prs} icon={<GitPullRequest />} />
         <Stat label="Contributed To:" data={user.help} icon={<Users />} />
-        <Stat label="Top Language:" data={'TypeScript'} icon={<Code2 />} />
+        <Stat label="Top Language:" data={user.language} icon={<Code2 />} />
       </div>
     </div>
   );
 };
 
-type StatProps = { label: string; data: string | number; icon: ReactElement };
+type StatProps = { label: string; data?: string | number; icon: ReactElement };
 const Stat = ({ label, data, icon }: StatProps) => (
   <div className="flex gap-2">
     <div className="flex items-center gap-2">
@@ -43,7 +43,7 @@ const Stat = ({ label, data, icon }: StatProps) => (
 export const getUser = async () => {
   const pastYear = new Date().setFullYear(new Date().getFullYear() - 1);
   const from = new Date(pastYear).toISOString();
-  const query = `query { user(login: "${env.GITHUB_USERNAME}") { contributions: contributionsCollection(from: "${from}") { totalCommitContributions restrictedContributionsCount } reposForStars: repositories(ownerAffiliations: OWNER last: 100 orderBy: { direction: DESC, field: STARGAZERS }) { nodes { stargazers { totalCount } } } pullRequests(first: 1) { totalCount } issues { totalCount } contributedTo: repositoriesContributedTo(first: 1 contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) { totalCount } } }`;
+  const query = `query { user(login: "${env.GITHUB_USERNAME}") { contributions: contributionsCollection(from: "${from}") { totalCommitContributions restrictedContributionsCount } reposForStars: repositories(ownerAffiliations: OWNER last: 100 orderBy: { direction: DESC, field: STARGAZERS }) { nodes { stargazers { totalCount } } } pullRequests(first: 1) { totalCount } issues { totalCount } contributedTo: repositoriesContributedTo(first: 1 contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) { totalCount }       reposForLanguage: repositories(isFork: false last: 30) { nodes { primaryLanguage { name } } } } }`;
   return octokit<{
     user: {
       contributions: ContributionsCollection;
@@ -51,9 +51,11 @@ export const getUser = async () => {
       pullRequests: PullRequestConnection;
       issues: IssueConnection;
       contributedTo: RepositoryConnection;
+      reposForLanguage: RepositoryConnection;
     };
   }>(query).then(({ user }) => {
     const stars = user.reposForStars.nodes as Array<Repository>;
+    const languages = user.reposForLanguage.nodes as Array<Repository>;
 
     return {
       commits:
@@ -63,6 +65,16 @@ export const getUser = async () => {
       issues: user.issues.totalCount,
       prs: user.pullRequests.totalCount,
       help: user.contributedTo.totalCount,
+      language: languages
+        .sort((a, b) => {
+          const aName = a.primaryLanguage?.name;
+          const bName = b.primaryLanguage?.name;
+          const _a = languages.filter((l) => l.primaryLanguage?.name === aName);
+          const _b = languages.filter((l) => l.primaryLanguage?.name === bName);
+
+          return _a.length - _b.length;
+        })
+        .pop()?.primaryLanguage?.name,
     };
   });
 };
